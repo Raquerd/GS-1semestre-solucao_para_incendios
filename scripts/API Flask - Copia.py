b2a364a11a727335c69b28def0e71953a00d3719
Davi Ferreira <davistudyreference@gmail.com>
2025-06-06 21:25:57 -0300

    MOD: Adicionado função de alerta por aplicativo na script API

--- scripts/API Flask - Copia.py
+++ scripts/API Flask - Copia.py
@@ -1,4 +1,4 @@
-import pandas as pd, joblib, os, requests
+import pandas as pd, joblib, os
 from flask import Flask, request, jsonify
 from datetime import datetime
 
@@ -6,6 +6,7 @@ path = "historico_incendios.xlsx"
 app = Flask(__name__)
 
 ml = joblib.load(r'C:\Users\Davi\Documents\Projetos\FIAP\FASE 4\Global Solutions\document\modelo_incendio.pkl') #<-Carregando modelo de Machine Learning
+
 
 def salvar_dados(dados, risco):
     df = pd.DataFrame([dados]) #<-Criando dataframe
@@ -20,27 +21,14 @@ def salvar_dados(dados, risco):
 
     df_final.to_excel(path, index=False) #<- Salva o arquivo
 
-def alerta(mensagem):
-    bot_token = 'token'
-    bot_id = 'bot_id'
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    data = {'bot_id': bot_id, 'text': mensagem}
-    requests.post(url, data=data)
-
 @app.route('/predict', methods =['POST'])
 def predict():
     data = request.get_json() #<- Recebe os dados
     df = pd.Dataframe(data) #<- Transforma os dados em dataframe
     pred = ml.predict(df)[0] #<- Faz a prevenção dos dados com base no modelo machine learning disponibilizado
+    return jsonify({'risco': pred}) #<- Envia a responsta da prevenção
     salvar_dados(df, pred) #<- Salva os dados
 
-    if pred == 'ALERTA: INCENDIO':
-        alerta('🚨 ALERTA! Incendio acontecendo no local.')
-    elif pred == 'ALTO':
-        alerta('🚨 ALERTA! Alto risco de incêndio detectado na sua região!')
-
-    return jsonify({'risco': pred}) #<- Envia a responsta da prevenção
-    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
